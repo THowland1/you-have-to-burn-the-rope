78d92ed630c0c4d1f6d1d4f73bc2fa543953d63e
Tom Howland <tomhowland9@gmail.com>
2022-04-28 23:30:38 +0100

    Think about it in terms of local and global positions

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const canvas = document.querySelector('canvas');
+const debugPlayer = document.querySelector('#debug-player');
+const debugOffset = document.querySelector('#debug-offset');
 const c = canvas.getContext('2d');
 
 canvas.width = 600;
 canvas.height = 400;
 const gravity = 1;
+const speed = 5;
+const offset = {
+    x: 0,
+    y: 50
+};
 
 class Player {
     constructor() {
-        this.position = {
-            x: 100,
+        this.globalPosition = {
+            x: 144,
             y: 100
         };
         this.velocity = {
@@ -19,17 +26,26 @@ class Player {
         this.height = 20;
     }
 
+    get left() { return this.globalPosition.x; }
+    get right() { return this.left + this.width; }
+    get top() { return this.globalPosition.y; }
+    get bottom() { return this.top + this.height; }
+
+    get localLeft() { return this.globalPosition.x - offset.x; }
+    get localRight() { return this.localLeft + this.width; }
+    get localTop() { return this.globalPosition.y - offset.y; }
+    get localBottom() { return this.localTop + this.height; }
+
     draw() {
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        c.fillRect(this.localLeft, this.localTop, this.width, this.height);
     }
-
     update() {
         this.draw();
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
+        this.globalPosition.x += this.velocity.x;
+        this.globalPosition.y += this.velocity.y;
 
-        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+        if (this.globalPosition.y + this.height + this.velocity.y <= canvas.height) {
             this.velocity.y += gravity;
         } else {
             this.velocity.y = 0;
@@ -38,16 +54,25 @@ class Player {
 }
 class Platform {
     constructor({ x, y, width, height }) {
-        this.position = {
+        this.globalPosition = {
             x,
             y
         };
         this.width = width;
         this.height = height;
     }
+    get left() { return this.globalPosition.x; }
+    get right() { return this.left + this.width; }
+    get top() { return this.globalPosition.y; }
+    get bottom() { return this.top + this.height; }
+
+    get localLeft() { return this.globalPosition.x - offset.x; }
+    get localRight() { return this.localLeft + this.width; }
+    get localTop() { return this.globalPosition.y - offset.y; }
+    get localBottom() { return this.localTop + this.height; }
     draw() {
         c.fillStyle = 'blue';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        c.fillRect(this.localLeft, this.localTop, this.width, this.height);
     }
 }
 const keys = {
@@ -65,49 +90,71 @@ function animate() {
     player.update();
     platforms.forEach(platform => platform.draw());
 
-    if (keys.right.pressed && player.position.x < 400) {
-        player.velocity.x = 5;
-    } else if (keys.left.pressed && player.position.x > 100) {
-        player.velocity.x = -5;
+    // # Update velocity for next frame
+
+    if (keys.right.pressed) {
+        player.velocity.x = speed;
+    } else if (keys.left.pressed) {
+        player.velocity.x = -speed;
     } else {
         player.velocity.x = 0;
+    }
 
-        if (keys.right.pressed) {
-            platforms.forEach(platform => platform.position.x -= 5);
-        } else if (keys.left.pressed) {
-            platforms.forEach(platform => platform.position.x += 5);
-        }
+    if (player.localRight > 400 && player.velocity.x > 0) {
+        offset.x += player.velocity.x;
+    } else if (player.localLeft < 100 && player.velocity.x < 0) {
+        offset.x += player.velocity.x;
+    }
+    if (player.localTop < 100 && player.velocity.y < 0) {
+        offset.y += player.velocity.y;
+    } else if (player.localBottom > 300 && player.velocity.y > 0) {
+        offset.y += player.velocity.y;
     }
 
     platforms.forEach(platform => {
-        if (player.position.y + player.height <= platform.position.y &&
-            player.position.y + player.height + player.velocity.y >= platform.position.y &&
-            player.position.x + player.width >= platform.position.x &&
-            player.position.x <= platform.position.x + platform.width
+        if (player.bottom <= platform.top &&
+            player.bottom + player.velocity.y >= platform.top &&
+            player.right >= platform.left &&
+            player.left <= platform.right
         ) {
             player.velocity.y = 0;
         }
-        if (player.position.x + player.width <= platform.position.x &&
-            player.position.x + player.width + player.velocity.x >= platform.position.x &&
-            player.position.y + player.height >= platform.position.y &&
-            player.position.y <= platform.position.y + platform.height
-        ) {
-            player.velocity.x = 0;
-        }
-        if (player.position.x >= platform.position.x + platform.width &&
-            player.position.x + player.velocity.x <= platform.position.x + platform.width &&
-            player.position.y + player.height >= platform.position.y &&
-            player.position.y <= platform.position.y + platform.height
-        ) {
-            player.velocity.x = 0;
+
+        if (player.bottom >= platform.top && player.top <= platform.bottom) {
+            if (player.right <= platform.left &&
+                player.right + player.velocity.x >= platform.left
+            ) {
+                player.velocity.x = 0;
+            }
+            if (player.left >= platform.right &&
+                player.left + player.velocity.x <= platform.right
+            ) {
+                player.velocity.x = 0;
+            }
         }
     });
+
+    // if (player.position.y < 100 && offset.y > -4321 && player.velocity.y < 0) {
+    //     offset.y += player.velocity.y;
+    //     player.velocity.y = 0;
+    // }
+    // if (player.position.y > 300 && offset.y < 0 && player.velocity.y > 0) {
+    //     offset.y += player.velocity.y;
+    //     player.velocity.y = 0;
+    // }
+
+    // debugPlayer.innerText = JSON.stringify(player);
 }
 
 
 
 const player = new Player();
-const platforms = [new Platform({ x: 200, y: 200, width: 200, height: 200 }), new Platform({ x: 500, y: 100, width: 200, height: 20 })];
+const platforms = [
+    new Platform({ x: 200, y: 200, width: 200, height: 200 }),
+    new Platform({ x: 500, y: 100, width: 200, height: 20 }),
+    new Platform({ x: 0, y: 300, width: 600, height: 32 }),
+    new Platform({ x: 96, y: 0, width: 32, height: 400 }),
+];
 player.draw();
 animate();
 
