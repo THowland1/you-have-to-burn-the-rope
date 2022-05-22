06b91ab3aeafb5d1ac2974f941230823546074e7
Tom Howland <tomhowland9@gmail.com>
2022-05-22 19:05:11 +0100

    Fix service worker

--- serviceworker.js
+++ serviceworker.js
@@ -121,23 +121,9 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', function (event) {
     // console.log('[Service Worker] Fetch', event.request.url);
-    if (event.request.mode === 'navigate') {
-        event.respondWith((async () => {
-            try {
-                const preloadResponse = await event.preloadResponse;
-                if (preloadResponse) {
-                    return preloadResponse;
-                }
-
-                const networkResponse = await fetch(event.request);
-                return networkResponse;
-            } catch (error) {
-                console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
-
-                const cache = await caches.open(CACHE_NAME);
-                const cachedResponse = await cache.match(OFFLINE_URL);
-                return cachedResponse;
-            }
-        })());
-    }
+    event.respondWith((async () => {
+        const cachedResponse = await caches.match(event.request);
+        console.log;
+        return cachedResponse || fetch(event.request);
+    })());
 });
